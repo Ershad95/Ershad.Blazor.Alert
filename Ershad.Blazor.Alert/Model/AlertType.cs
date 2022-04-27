363e3405a168fe5a1661c0de4e418de65a18a898
Ershad Raoufi <waveershad21@gmail.com>
2022-04-27 17:21:13 +0430

    Init

---
+++ Ershad.Blazor.Alert/Model/AlertType.cs
@@ -0,0 +1,17 @@
+﻿namespace Ershad.Blazor.Alert.Model
+{
+    public enum TypeAlert
+    {
+        Danger = -1,
+        Success = 1,
+        Info = 0,
+        Warning = 2
+    }
+    public enum AnimationType
+    {
+        Bounce=1,
+        Flash = 2,
+        Pulse=3,
+        HeartBeat=4
+    }
+}
